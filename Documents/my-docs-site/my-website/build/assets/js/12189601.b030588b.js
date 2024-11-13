637c0064b9ab6701f1f46b7c2a03fd36cca9d453
regina <huang.rong@northeastern.edu>
2024-11-13 04:04:56 -0500

    Add generated build files for GitHub Pages

---
+++ Documents/my-docs-site/my-website/build/assets/js/12189601.b030588b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[9289],{3231:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/Regina-Dev-Blog/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
